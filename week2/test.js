77bb6919bd053b2abfa83bad1475270a9834a92e
elvanrafif <terlanjurbuta@gmail.com>
2020-02-25 15:18:26 +0700

    pseudocode

---
+++ week2/test.js
@@ -0,0 +1,35 @@
+// var kabisat = 2024
+// if (kabisat %4 == 0 && kabisat %100 != 0)
+// console.log(kabisat + " adalah tahun kabisat")
+// else 
+// console.log(kabisat + " adalah bukan tahun kabisat")
+
+
+// var cucian = 1
+// while (cucian <= 20){
+//     console.log('ini adalah baju ke-' + cucian)
+//     cucian++
+// if (cucian > 20)
+//     console.log('MESIN MENYALA')
+// else
+//     console.log('MESIN MATI')}
+
+// var siswa =  1
+// var panjang = 1
+// var pendek = 2
+// if (siswa == panjang)
+//     console.log("kalian dihukum")
+// else
+//     console.log("kalian dipuji")
+
+var i = 0;
+while(i < 40 )
+i++
+console.log(i);
+
+var i = 24;
+if(i % 2 ===0) {
+  console.log("mendapat pujian dari guru");
+} else {
+  console.log("mendapat hukuman dari guru");
+}
