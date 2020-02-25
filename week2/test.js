5ea60ae52c7e1f375c206aea74aae65a898b9147
elvanrafif <terlanjurbuta@gmail.com>
2020-02-25 20:08:51 +0700

    tugas update

--- week2/test.js
+++ week2/test.js
@@ -22,14 +22,53 @@
 // else
 //     console.log("kalian dipuji")
 
-var i = 0;
-while(i < 40 )
-i++
-console.log(i);
+// var i = 0;
+// while(i < 40 )
+// i++
+// console.log(i);
 
-var i = 24;
-if(i % 2 ===0) {
-  console.log("mendapat pujian dari guru");
-} else {
-  console.log("mendapat hukuman dari guru");
-}
+// var i = 24;
+// if(i % 2 ===0) {
+//   console.log("mendapat pujian dari guru");
+// } else {
+//   console.log("mendapat hukuman dari guru");
+// }
+
+// var nama = "Mikael";
+// var peran = "";
+
+// if( nama === "" && peran === "") {
+//     console.log("Nama harus diisi!");
+// }
+
+// if( nama === "Mikael") {
+//   console.log("Halo " +nama+", pilih peranmu untuk memulai game!");
+// }
+
+// if( nama === "Nina" && peran === "Ksatria") {
+//     console.log("Selamat datang di dunia Proxytia "+nama+". Halo "+peran+" "+nama+", kamu dapat menyerang dengan senjatamu!");
+// }
+
+// if(nama === "Danu" && peran === "Tabib") {
+//     console.log("Selamat datang di dunia Proxytia, "+nama+". Halo "+peran+" "+nama+", kamu akan membantu temanmu yang terluka.");
+// }
+
+// if( nama === "Zero" && peran === "Penyihir") {
+//     console.log("Selamat datang di dunia Proxytia, "+nama+". Halo "+peran+" "+nama+", ciptakan keajaiban yang membantu kemenanganmu!");
+// }
+
+var nama = "Elvan";
+var peran = "Penyihir";
+  
+  if (nama === ""){
+    console.log("Nama harus diisi!")
+} else if (peran === ""){
+    console.log("Halo " + nama +", Pilih peranmu untuk memulai game!")
+} else if (peran === "Ksatria"){
+    console.log("Selamat datang di Dunia Proxytia, " + nama +". Halo Ksatria " + nama+", kamu dapat menyerang dengan senjatamu!")
+} else if (peran === "Tabib")  {
+    console.log("Selamat datang di Dunia Proxytia, " + nama +". Halo Tabib " + nama+", kamu akan membantu temanmu yang terluka")
+} else if (peran === "Penyihir"){
+    console.log("Selamat datang di Dunia Proxytia, " + nama +". Halo Penyihir " + nama+", ciptakan keajaiban yang membantu kemenanganmu")
+} else
+    console.log("Halo " + nama + " Peran tersebut tidak ada")
